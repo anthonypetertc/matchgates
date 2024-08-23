b14f98e7120fe177e8036e4faa89c4a177f40539
Christopher Cade <ccade670@gmail.com>
2024-08-23 19:28:05 +0200

    Update

--- examples/scripts/noisy_circuits.py
+++ examples/scripts/noisy_circuits.py
@@ -1,52 +1,80 @@
 from time import perf_counter
+import pandas as pd
 
 from matchgates.generators import XY_circuit
 from matchgates import Observable, ProductState
 from matchgates import SingleGateNoise
 
-n_qubits = 60  # number of qubits
-dt = 0.1  # trotter step time interval
-J = 0.5  # coupling strength
-h = 0.23  # tranverse field
-trotter_steps = 30  # number of trotter steps
 
-p = 0.002  # Noise strength
+if __name__=="__main__":
 
-n_jobs = 1  # Number of things to run in parallel
-repetitions = 100  # Number of repetitions (per exp. val) 
+    n_qubits = 60  # number of qubits
+    dt = 0.1  # trotter step time interval
+    J = 0.5  # coupling strength
+    h = 0.23  # tranverse field
+    trotter_steps = 30  # number of trotter steps
 
+    qubit_for_observables = n_qubits // 2
 
-# generate the XY circuit.
-xy = XY_circuit(n_qubits=n_qubits, dt=dt, J=J, h=h, trotter_steps=trotter_steps)
+    p = 0.002  # Noise strength
 
-# Make observables
-obsz = Observable(name="Z", qubits=[8], n_qubits=n_qubits)
-obsyx = Observable(name="YX", qubits=[9, 10], n_qubits=n_qubits)
+    # Total number of runs will be repetitions * iterations
+    n_jobs = 32  # Number of cores to use
+    repetitions = 960  # Number of repetitions
+    iterations = 100  # Number of iterations per repetition
 
-# prepare initial product state.
-initial_state = ProductState.neel(even=True, n_qubits=n_qubits)
+    # generate the XY circuit.
+    xy = XY_circuit(n_qubits=n_qubits, dt=dt, J=J, h=h, trotter_steps=trotter_steps)
 
-noise_channel = SingleGateNoise.matchgate_depolarizing(p=p)
-# matchgate depolarizing is equal probability of perturbation by 
-# any matchgate Pauli (Z, ZZ, XY, YX, YY, XX). 
+    # Make observables
+    obsz = Observable(name="Z", qubits=[qubit_for_observables], n_qubits=n_qubits)
+    obsyx = Observable(name="YX", qubits=[qubit_for_observables, qubit_for_observables + 1], n_qubits=n_qubits)
 
-noisy_xy = xy.add_two_qubit_uniform_noise(noise_channel)  # Add noise to circuit.
+    # Prepare initial product state.
+    initial_state = ProductState.neel(even=True, n_qubits=n_qubits)
 
-# Simulate
-start = perf_counter()
-results, hash_counts = noisy_xy.simulate_noisy(n_jobs, [obsz, obsyx], repetitions, initial_state)
-end = perf_counter()
+    # Create noise channel
+    noise_channel = SingleGateNoise.matchgate_depolarizing(p=p)
+    # matchgate depolarizing is equal probability of perturbation by 
+    # any matchgate Pauli (Z, ZZ, XY, YX, YY, XX). 
 
-print("Results for XY circuit with noise:", results)
+    # Add noise to circuit
+    noisy_xy = xy.add_two_qubit_uniform_noise(noise_channel) 
 
-# Post-process
-hash_counts_sorted_by_freq = sorted(list(hash_counts.values()), reverse=True)
-trajectories_sorted_by_freq = sorted([(k, v,) for k, v in hash_counts.items()], key=lambda x: x[1], reverse=True)
-all_I_string = "I" * len(list(hash_counts.keys())[0])
-print("Sorted hash counts:")
-print(hash_counts_sorted_by_freq)
-print(f"Frequency of all-identity trajectory: {hash_counts[all_I_string]}")
-print(f"Most common string: {trajectories_sorted_by_freq[0][0]}")
-print(f"with frequency {hash_counts[trajectories_sorted_by_freq[0][0]]}")
+    # Filename 
+    filename = f"./XY_p{p}_N{n_qubits}_J{J}_h{h}_steps{trotter_steps}_rep{repetitions}.csv"
 
-print(f"Avg. time per run: {(end - start) / repetitions}s")
+    print(f"Running {repetitions} over {n_jobs} cores for {iterations} iterations.")
+    print(f"Saving data to {filename}")
+    print()
+
+    # Run (distributed) noisy emulations for each iteration
+    start_start = perf_counter()
+    for i in range(iterations):
+        start = perf_counter()
+        results, _ = noisy_xy.simulate_noisy(n_jobs, [obsz, obsyx], repetitions, initial_state)
+        end = perf_counter()
+
+        print(f"Finished {i+1} / {iterations} iterations  |  Total time: {end - start}s  |  Avg. time per run: {(end - start) / repetitions}s")
+
+        new_results = {}
+        for key in results.keys():
+            new_results[key] = [results[key]]
+        df = pd.DataFrame(new_results)
+
+        if i == 0:
+            df.to_csv(
+                filename,
+                mode="w",
+                header=True,
+                index=False,
+            )
+        else:
+            df.to_csv(
+                filename,
+                mode="a",
+                header=False,
+                index=False,
+            )
+
+    print(f"All finished in {perf_counter() - start_start}s.")
